9c8a8f556b25c83daa54927f73ab0a0ae554ca7a
bjstudiosgit <bjstudiosspam@gmail.com>
2026-04-14 16:19:16 +0100

    Delete src directory

--- src/game/Physics.ts
+++
@@ -1,8 +0,0 @@
-import { CONFIG } from './Constants';
-import { Entity } from './Types';
-
-export function applyPhysics(entity: Entity) {
-    entity.vy += CONFIG.gravity;
-    entity.x += entity.vx;
-    entity.y += entity.vy;
-}
